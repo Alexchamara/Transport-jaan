e814ba64882bd646c2a002c84388d750672d1c93
Alex Chamara <cb011374@students.apiit.lk>
2025-08-22 16:32:10 +0530

    auth

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
 use App\Models\FreightQuote;
 use App\Mail\FreightQuoteSubmitted;
 use Inertia\Inertia;
