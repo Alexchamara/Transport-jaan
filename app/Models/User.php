1ba6e0f06da4e6c674130cd5a8ccc1bff7ab3276
Prasantha <prasantha20011216@gmail.com>
2025-08-25 16:10:33 +0530

    user.php creation

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,7 +58,7 @@ class User extends Authenticatable
 
     public function vehicleReviews()
 {
-    return $this->hasMany(VehicleReview::class, 'client_id', 'id');
+       return $this->hasMany(VehicleReview::class);
 }
 
 }
