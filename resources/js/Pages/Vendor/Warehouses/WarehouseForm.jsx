817abb9569a7342e7e33d2389f7733c5f8279e5b
Alex Chamara <cb011374@students.apiit.lk>
2025-09-12 16:15:11 +0530

    refactor: Update asset import paths for consistency across components

--- resources/js/Pages/Vendor/Warehouses/WarehouseForm.jsx
+++ resources/js/Pages/Vendor/Warehouses/WarehouseForm.jsx
@@ -10,8 +10,7 @@ const WAREHOUSE_TYPES = [
 
 const PRICING_MODELS = [
     { value: 'hourly', label: 'Per Hour' },
-    { value: 'daily', l                                value={data.base_price}
-                                onChange={e => setData('base_price', e.target.value)}el: 'Per Day' },
+    { value: 'daily', label: 'Per Day' },
     { value: 'monthly', label: 'Per Month' },
 ];
 
--- resources/js/Pages/Web/components/vendors/units/ReviewsTab.jsx
+++ resources/js/Pages/Web/components/vendors/units/ReviewsTab.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReviewSection from "../../vehicleDetails/ReviewSection";
-import starSec from "../../../assets/landVehicleDetails/starsSec.svg";
+// Adjust depth: units (this folder) -> vendors (..1) -> components (..2) then into assets
+import starSec from "../../../../assets/landVehicleDetails/starsSec.svg";
 
 const ReviewsTab = () => (
     <div className="poppins">
