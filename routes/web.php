b240f78a7d7209911bdfa07ee681bc3971099382
ujithe <ujithegodawaththa@gmail.com>
2025-09-17 09:09:22 +0530

    Enhance SideMenu component: add dynamic active subsection handling and navigation links

--- routes/web.php
+++ routes/web.php
@@ -505,6 +505,26 @@ Route::get('/warehouse/bookings', function () {
 Route::get('/SuperAdmin/Vehicles', function () {
     return Inertia::render('Web/home/SuperAdmin/Vehicles');
 })->name('SuperAdmin.Vehicles');
+Route::get('/SuperAdmin/Vehicles', function () {
+    return Inertia::render('Web/home/SuperAdmin/Vehicles');
+})->name('SuperAdmin.Vehicles');
+
+// Route::get('/SuperAdmin/LandVehicleDetails', function () {
+//     return Inertia::render('Web/home/SuperAdmin/LandVehicleDetails');
+// })->name('SuperAdmin.LandVehicleDetails');
+
+// Route::get('/SuperAdmin/SeaVehicleDetails', function () {
+//     return Inertia::render('Web/home/SuperAdmin/SeaVehicleDetails');
+// })->name('SuperAdmin.SeaVehicleDetails');
+
+// Route::get('/SuperAdmin/AirVehicleDetails', function () {
+//     return Inertia::render('Web/home/SuperAdmin/AirVehicleDetails');
+// })->name('SuperAdmin.AirVehicleDetails');
+
+Route::get('/SuperAdmin/Vender', function () {
+    return Inertia::render('Web/home/SuperAdmin/NewVender');
+})->name('SuperAdmin.NewVender');
+
 
 // Route::get('/SuperAdmin/LandVehicleDetails', function () {
 //     return Inertia::render('Web/home/SuperAdmin/LandVehicleDetails');
