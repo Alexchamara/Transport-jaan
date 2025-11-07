53e2cdf739d3ff7b4873f4222047aaa0b1b7a8b9
kevinbrinsly <brinslykevin@gmail.com>
2025-11-07 11:26:59 +0530

    UI Changes

--- resources/js/Pages/Web/components/vendors/dashboard/DashContent.jsx
+++ resources/js/Pages/Web/components/vendors/dashboard/DashContent.jsx
@@ -55,8 +55,8 @@ const DashContent = ({
       <div className="flex xl:flex-row flex-col gap-5 justify-between items-center">
         <h1 className="figtree text-[35px] font-[700]">Dashboard</h1>
         <div className="flex flex-row gap-5">
-          <NotificationDropdown bellIcon={bell} unreadCount={unreadNotifications} />
-          <div className="size-[60px] rounded-[10px] bg-[#E8EBEF] flex justify-center items-center">
+          {/* <NotificationDropdown bellIcon={bell} unreadCount={unreadNotifications} /> */}
+          {/* <div className="size-[60px] rounded-[10px] bg-[#E8EBEF] flex justify-center items-center">
             <img src={proPic} />
           </div>
 
@@ -65,7 +65,7 @@ const DashContent = ({
             <h1 className="text-[16px] font-[600] text-[#7B7B7A]">
               {vendorUser?.role ?? "Vendor"}
             </h1>
-          </div>
+          </div> */}
         </div>
       </div>
 
@@ -77,15 +77,15 @@ const DashContent = ({
             <div className="flex flex-col gap-5">
               <div className="flex xl:flex-row flex-col gap-5 justify-between w-full">
                 {/* Total Revenue */}
-                <div className="min-w-[360px] w-full min-h-[91px] bg-white rounded-[8px] flex justify-between items-center gap-2 px-5 py-2"
+                <div className="min-w-[300px] w-full min-h-[91px] bg-white rounded-[8px] flex justify-between items-center gap-2 px-5 py-2"
                      style={{ boxShadow: "4px 4px 4px #0000001A" }}>
                   <div className="flex flex-row gap-5 items-center">
                     <div className="size-[50px] bg-[#D8E4F2] rounded-full flex justify-center items-center">
                       <img src={dollarIcon} />
                     </div>
                     <div>
-                      <h1 className="text-[16px] font-[500] text-[#7B7B7A]">Total Revenue</h1>
-                      <h1 className="text-[26px] font-[700]">{fmtMoney(cards?.totalRevenue ?? 0)}</h1>
+                      <h1 className="text-[14px] font-[500] text-[#7B7B7A]">Total Revenue</h1>
+                      <h1 className="text-[24px] font-[700]">{fmtMoney(cards?.totalRevenue ?? 0)}</h1>
                     </div>
                   </div>
                   <div className="flex flex-col gap-2 items-end text-[14px] font-[500]">
@@ -98,15 +98,15 @@ const DashContent = ({
                 </div>
 
                 {/* New Bookings */}
-                <div className="min-w-[360px] w-full min-h-[91px] bg-white rounded-[8px] flex justify-between items-center gap-2 px-5 py-2"
+                <div className="min-w-[300px] w-full min-h-[91px] bg-white rounded-[8px] flex justify-between items-center gap-2 px-5 py-2"
                      style={{ boxShadow: "4px 4px 4px #0000001A" }}>
                   <div className="flex flex-row gap-5 items-center">
                     <div className="size-[50px] bg-[#D8E4F2] rounded-full flex justify-center items-center">
                       <img src={bookingIcon} />
                     </div>
                     <div>
-                      <h1 className="text-[16px] font-[500] text-[#7B7B7A]">New Bookings</h1>
-                      <h1 className="text-[26px] font-[700]">{cards?.newBookings ?? 0}</h1>
+                      <h1 className="text-[14px] font-[500] text-[#7B7B7A]">New Bookings</h1>
+                      <h1 className="text-[24px] font-[700]">{cards?.newBookings ?? 0}</h1>
                     </div>
                   </div>
                   <div className="flex flex-col gap-2 items-end text-[14px] font-[500]">
@@ -121,15 +121,15 @@ const DashContent = ({
 
               <div className="flex xl:flex-row flex-col gap-5 w-full">
                 {/* Rented Cars */}
-                <div className="min-w-[360px] w-full min-h-[91px] bg-white rounded-[8px] flex justify-between items-center gap-2 px-5 py-2"
+                <div className="min-w-[300px] w-full min-h-[91px] bg-white rounded-[8px] flex justify-between items-center gap-2 px-5 py-2"
                      style={{ boxShadow: "4px 4px 4px #0000001A" }}>
                   <div className="flex flex-row gap-5 items-center">
                     <div className="size-[50px] bg-[#D8E4F2] rounded-full flex justify-center items-center">
                       <img src={wheelIcon} />
                     </div>
                     <div>
-                      <h1 className="text-[16px] font-[500] text-[#7B7B7A]">Rented Cars</h1>
-                      <h1 className="text-[26px] font-[700]">{cards?.rentedCars ?? 0} Units</h1>
+                      <h1 className="text-[14px] font-[500] text-[#7B7B7A]">Rented Cars</h1>
+                      <h1 className="text-[24px] font-[700]">{cards?.rentedCars ?? 0} Units</h1>
                     </div>
                   </div>
                   <div className="flex flex-col gap-2 items-end text-[14px] font-[500]">
@@ -142,15 +142,15 @@ const DashContent = ({
                 </div>
 
                 {/* Total Cars */}
-                <div className="min-w-[360px] min-h-[91px] w-full bg-white rounded-[8px] flex justify-between items-center gap-2 px-5 py-2"
+                <div className="min-w-[300px] min-h-[91px] w-full bg-white rounded-[8px] flex justify-between items-center gap-2 px-5 py-2"
                      style={{ boxShadow: "4px 4px 4px #0000001A" }}>
                   <div className="flex flex-row gap-5 items-center">
                     <div className="size-[50px] bg-[#D8E4F2] rounded-full flex justify-center items-center">
                       <img src={carIcon} />
                     </div>
                     <div>
-                      <h1 className="text-[16px] font-[500] text-[#7B7B7A]">Total Cars</h1>
-                      <h1 className="text-[26px] font-[700]">{cards?.totalCars ?? 0} Units</h1>
+                      <h1 className="text-[14px] font-[500] text-[#7B7B7A]">Total Cars</h1>
+                      <h1 className="text-[24px] font-[700]">{cards?.totalCars ?? 0} Units</h1>
                     </div>
                   </div>
                   <div className="flex flex-col gap-2 items-end text-[14px] font-[500]">
@@ -165,7 +165,7 @@ const DashContent = ({
             </div>
 
             {/* Booking Overview */}
-            <div className="min-w-[742px] h-auto bg-white flex flex-col justify-center items-center rounded-[10px] py-10 px-10"
+            <div className="min-w-auto h-auto bg-white flex flex-col justify-center items-center rounded-[10px] py-10 px-10"
                  style={{ boxShadow: "4px 4px 4px #0000001A" }}>
               <div className="flex flex-row items-center justify-between mb-16 w-full">
                 <h1 className="text-[24px] font-[700]">Booking Overview</h1>
@@ -180,7 +180,7 @@ const DashContent = ({
             </div>
 
             {/* Earning Summary */}
-            <div className="min-w-[742px] min-h-[381px] bg-white rounded-[10px] py-10 px-10"
+            <div className="min-w-auto min-h-[381px] bg-white rounded-[10px] py-10 px-10"
                  style={{ boxShadow: "4px 4px 4px #0000001A" }}>
               <div className="flex flex-row items-center justify-between mb-12 w-full">
                 <h1 className="text-[24px] font-[700]">Earning Summary</h1>
@@ -198,18 +198,18 @@ const DashContent = ({
           {/* Right */}
           <div className="flex flex-col items-center gap-5 w-full">
             {/* Car Availability */}
-            <div className="min-w-[349px] w-full min-h-[206px] bg-[#D8E4F2] flex flex-col px-10 py-5 justify-center items-center rounded-[10px]"
+            <div className="min-w-auto w-full xl:min-h-[206px] bg-[#D8E4F2] flex flex-col px-10 py-5 justify-center items-center rounded-[10px]"
                  style={{ boxShadow: "4px 4px 4px #0000001A" }}>
               <h1 className="text-[24px] font-[700] mb-3">Car Availability</h1>
               <div className="flex flex-col gap-3 w-full items-center">
-                <div className="w-[283px] h-[35px] flex flex-row items-center gap-2 rounded-[6px] px-3 py-2 bg-white">
+                <div className="xl:w-[283px] xl:h-[35px] flex flex-row items-center gap-2 rounded-[6px] px-3 py-2 bg-white">
                   <img src={car} className="size-[20px]" />
                   <input className="w-full outline-none bg-transparent border-0 focus:ring-0" placeholder="Car Type" />
                   <img src={miniDownArrow} />
                 </div>
 
                 <div className="flex flex-row gap-3">
-                  <div className="w-[137px] h-[35px] bg-white rounded-[6px] flex flex-row items-center gap-2 py-2 px-3">
+                  <div className="xl:w-[137px] xl:h-[35px] bg-white rounded-[6px] flex flex-row items-center gap-2 py-2 px-3">
                     <img src={date} className="size-[20px]" />
                     <input className="w-full outline-none bg-transparent border-0 focus:ring-0" placeholder="Start date" />
                   </div>
@@ -219,14 +219,14 @@ const DashContent = ({
                   </div>
                 </div>
 
-                <button className="w-[283px] h-[40px] bg-[#0955AC] rounded-[6px] text-[16px] font-[700] text-white border-0 focus:ring-0">
+                <button className="xl:w-[283px] xl:h-[40px] bg-[#0955AC] rounded-[6px] text-[16px] font-[700] text-white border-0 focus:ring-0">
                   Check Availability
                 </button>
               </div>
             </div>
 
             {/* Real Status */}
-            <div className="min-w-[349px] w-full min-h-[427px] bg-white rounded-[10px] py-5 px-10"
+            <div className="min-w-auto w-full min-h-[427px] bg-white rounded-[10px] py-5 px-10"
                  style={{ boxShadow: "4px 4px 4px #0000001A" }}>
               <div className="flex flex-row items-center justify-between w-full">
                 <h1 className="text-[24px] font-[700]">Real Status</h1>
@@ -241,7 +241,7 @@ const DashContent = ({
             </div>
 
             {/* Reminders */}
-            <div className="min-w-[349px] w-full min-h-[335px] bg-white rounded-[10px] py-5 px-10"
+            <div className="min-w-auto w-full min-h-[335px] bg-white rounded-[10px] py-5 px-10"
                  style={{ boxShadow: "4px 4px 4px #0000001A" }}>
               <div className="flex flex-row items-center justify-between w-full">
                 <h1 className="text-[24px] font-[700]">Reminders</h1>
@@ -280,7 +280,7 @@ const DashContent = ({
 
         {/* Car types + Recent activities */}
         <div className="flex flex-col xl:flex-row gap-5 justify-between">
-          <div className="min-w-[500px] w-full min-h-[858px] bg-white rounded-[10px] px-10 py-10"
+          <div className="min-w-auto w-full min-h-[858px] bg-white rounded-[10px] px-10 py-10"
                style={{ boxShadow: "4px 4px 4px #0000001A" }}>
             <div className="flex flex-row justify-between items-center">
               <h1 className="text-[24px] font-[700]">Car types</h1>
