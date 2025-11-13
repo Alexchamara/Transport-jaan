d0d6b292cf55ae9f36494bb166cbc73c56214c25
pathum lakshan <123624162+pathumlak@users.noreply.github.com>
2025-11-13 11:21:57 +0530

    Remove unused search icon from ClientHeader component

--- resources/js/Pages/Web/home/client/ClientHeader.jsx
+++ resources/js/Pages/Web/home/client/ClientHeader.jsx
@@ -72,12 +72,6 @@ const ClientHeader = () => {
                 <div className="md:flex hidden flex-row gap-5 justify-center items-center">
                     <div className="size-[27px] md:size-[55px] rounded-full bg-[#E8EBEF] flex justify-center items-center">
                         <img
-                            src={search}
-                            className="size-[18px] md:w-[24px] md:h-[23px]"
-                        />
-                    </div>
-                    <div className="size-[27px] md:size-[55px] rounded-full bg-[#E8EBEF] flex justify-center items-center">
-                        <img
                             src={bell}
                             className="size-[18px] md:w-[24px] md:h-[23px]"
                         />
