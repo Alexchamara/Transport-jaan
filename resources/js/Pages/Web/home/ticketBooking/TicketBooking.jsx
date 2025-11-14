cd7bc376bb7964c9061b402a34531deff9c619a6
ujithe <ujithegodawaththa@gmail.com>
2025-11-14 16:48:55 +0530

    Ticket booking changes

--- resources/js/Pages/Web/home/ticketBooking/TicketBooking.jsx
+++ resources/js/Pages/Web/home/ticketBooking/TicketBooking.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import Header from '../../home/client/ClientHeader'
-import Hero from '../../components/flight/Hero'
-import ContentOne from '../../components/flight/ContentOne'
-import Why from '../../components/flight/Why'
-import Recommended from '../../components/flight/Recommended'
-import EasyPayment from '../../components/flight/EasyPayment'
+import React from "react";
+import { usePage } from "@inertiajs/react";
+
+import Header from "../../home/client/ClientHeader";
+import Hero from "../../components/flight/Hero";
+import ContentOne from "../../components/flight/ContentOne";
+import Why from "../../components/flight/Why";
+import Recommended from "../../components/flight/Recommended";
+import EasyPayment from "../../components/flight/EasyPayment";
 import FAQ from "../../components/freight/FAQ";
 import Review from "../../components/freight/Review";
 import Footer from "../../layouts/Footer";
 
 const TicketBooking = () => {
+  const { url } = usePage(); // e.g. "/ticketBooking?type=flight"
+
+  let initialType = "flight"; // default
+
+  if (typeof url === "string") {
+    const parts = url.split("?");
+    if (parts.length > 1) {
+      const params = new URLSearchParams(parts[1]);
+      const type = params.get("type"); // "flight" | "train" | "bus" | null
+      if (type === "train" || type === "bus" || type === "flight") {
+        initialType = type;
+      }
+    }
+  }
+
   return (
     <div>
       <Header />
-      <Hero />
+      {/* Pass the decided type to Hero */}
+      <Hero initialType={initialType} />
       <ContentOne />
       <Why />
       <Recommended />
@@ -22,7 +40,7 @@ const TicketBooking = () => {
       <Review />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default TicketBooking;
