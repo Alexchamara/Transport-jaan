89b54625f5516d8af7126a26a238fb92e20c2d92
MenalinR <142731196+MenalinR@users.noreply.github.com>
2025-11-20 11:34:31 +0530

    Hide SideMenu in Freight  dashboard responsive view

--- resources/js/Pages/Web/home/vendors/freight/Booking.jsx
+++ resources/js/Pages/Web/home/vendors/freight/Booking.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import SideMenu from "../../../components/vendors/freight/SideMenu";
 import BookingContent from "../../../components/vendors/freight/bookings/BookingContent";
+import { Menu } from "lucide-react";
 
 const Booking = () => {
-    return (
-        <div className="bg-[#E5E5E5] h-auto">
-            <div className="flex flex-row gap-10 h-auto">
-                <SideMenu />
-                <BookingContent />
-            </div>
+  const [isOpen, setIsOpen] = useState(false); // Mobile sidebar toggle
+
+  return (
+    <div className="bg-[#E5E5E5] min-h-screen">
+      <div className="flex flex-row gap-10 h-auto">
+        {/* Toggle Button for mobile */}
+        <button
+          className="lg:hidden p-2 m-2 fixed left-2 top-2 z-50 bg-white rounded-full shadow"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <Menu size={22} />
+        </button>
+
+        {/* Side Menu */}
+        <div
+          className={`fixed lg:static top-0 left-0 h-full z-40 transition-transform duration-300
+          ${isOpen ? "translate-x-0" : "-translate-x-full"}
+          lg:translate-x-0 bg-white shadow lg:shadow-none`}
+        >
+          <SideMenu />
         </div>
-    );
+
+        {/* Main Content */}
+        <div className="flex-1">
+          <BookingContent />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Booking;
