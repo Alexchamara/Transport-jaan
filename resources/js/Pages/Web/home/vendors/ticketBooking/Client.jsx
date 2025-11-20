d74d0578b46e014327ffabd715a2e313e989ca5f
MenalinR <142731196+MenalinR@users.noreply.github.com>
2025-11-20 12:21:01 +0530

    Hide SideMenu in Ticket Booking  dashboard-responsive view

--- resources/js/Pages/Web/home/vendors/ticketBooking/Client.jsx
+++ resources/js/Pages/Web/home/vendors/ticketBooking/Client.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import SideMenu from "../../../components/vendors/ticketBooking/SideMenu";
 import ClientContent from "../../../components/vendors/ticketBooking/clients/ClientContent";
+import { Menu } from "lucide-react";
 
 const Client = () => {
-    return (
-        <div className="bg-[#E5E5E5] h-auto">
-            <div className="flex flex-row gap-10 h-auto">
-                <SideMenu />
-                <ClientContent />
-            </div>
-        </div>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="bg-[#E5E5E5] min-h-screen flex">
+
+      {/* Mobile toggle button */}
+      <button
+        className="lg:hidden p-2 m-2 fixed left-2 top-2 z-50 bg-white rounded-full shadow"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <Menu size={22} />
+      </button>
+
+      {/* Side Menu */}
+      <div
+        className={`fixed lg:static top-0 left-0 h-full z-40 transition-transform duration-300 
+        ${isOpen ? "translate-x-0" : "-translate-x-full"} 
+        lg:translate-x-0 bg-white shadow lg:shadow-none`}
+      >
+        <SideMenu />
+      </div>
+
+      {/* Page Content */}
+      <div className="flex-1 p-4 lg:p-10">
+        <ClientContent />
+      </div>
+    </div>
+  );
 };
 
 export default Client;
