3a6d54d6e9ecb8dc3c8b0645e85d9e9968a704d2
Alex Chamara <cb011374@students.apiit.lk>
2025-12-09 11:42:07 +0530

    bug 6

--- app/Http/Controllers/WarehouseControllers/Vendor/WarehouseReservationController.php
+++ app/Http/Controllers/WarehouseControllers/Vendor/WarehouseReservationController.php
@@ -38,7 +38,7 @@ class WarehouseReservationController extends Controller
             // ");
 
             // select all from user table
-            $reservations = DB::select("SELECT * FROM warehouse_bookings");
+            $reservations = DB::select("SELECT * FROM warehouse_units");
             
             return response()->json([
                 'success' => true,
