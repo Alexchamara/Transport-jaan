ce3e3bc126ddd7bec7e59644a186fdf8c199e4f4
Alex Chamara <cb011374@students.apiit.lk>
2025-12-30 13:03:34 +0530

    fix

--- resources/js/Pages/Demo/BookingReferenceDemo.jsx
+++ resources/js/Pages/Demo/BookingReferenceDemo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import BookingReferenceDisplay from '../../../Components/BookingReferenceDisplay';
-import BookingReferenceInput from '../../../Components/BookingReferenceInput';
+import BookingReferenceDisplay from '../../Components/BookingReferenceDisplay';
+import BookingReferenceInput from '../../Components/BookingReferenceInput';
 
 /**
  * Booking Reference Components Demo Page
