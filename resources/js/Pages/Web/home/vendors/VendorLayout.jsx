484a1d0a3dcf7f4ad5264d07f55c142cb31cda5d
G K M Perera <158121610+PereraMadhushani@users.noreply.github.com>
2026-03-03 15:39:12 +0530

    Add validatio to vendor profile

--- resources/js/Pages/Web/home/vendors/VendorLayout.jsx
+++ resources/js/Pages/Web/home/vendors/VendorLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { usePage } from "@inertiajs/react";
 import { Menu } from "lucide-react";
-import SideMenu from "../../components/vendors/SideMenu";
+import SideMenu from "./allBookings/SideMenu";
 import ServiceNavBar from "../../../../Components/vendors/ServiceNavBar";
 
 /**
