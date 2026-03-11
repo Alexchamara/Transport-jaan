635f89a355eae2bc2ba83184bc287fc02119c32b
MenalinR <142731196+MenalinR@users.noreply.github.com>
2026-03-11 13:31:43 +0530

    removed searchbar

--- resources/js/Pages/Web/home/vendors/allBookings/VendorAllBookings.jsx
+++ resources/js/Pages/Web/home/vendors/allBookings/VendorAllBookings.jsx
@@ -562,18 +562,7 @@ const VendorAllBookings = ({
                                         </div>
                                     </div>
 
-                                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 mb-4">
-                                        {/* Search */}
-                                        <div className="flex flex-col gap-2">
-                                            <label className="text-[12px] font-[600] text-gray-700">Search</label>
-                                            <input
-                                                type="text"
-                                                value={searchQuery}
-                                                onChange={(e) => setSearchQuery(e.target.value)}
-                                                placeholder="Customer, vehicle, ref..."
-                                                className="w-full px-3 py-2 border border-gray-300 rounded-[6px] text-[14px] focus:outline-none focus:ring-2 focus:ring-[#0955AC]"
-                                            />
-                                        </div>
+                                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
 
                                         {/* Status */}
                                         <div className="flex flex-col gap-2">
