b5c93b1be4d310cdca570268163eaf3da3ea66c8
MenalinR <142731196+MenalinR@users.noreply.github.com>
2026-03-12 10:46:17 +0530

    redirect done button

--- resources/js/Pages/Web/home/land/Summary.jsx
+++ resources/js/Pages/Web/home/land/Summary.jsx
@@ -191,7 +191,8 @@ const Summary = () => {
 
             {/* travel summary */}
             <div>
-              <h1 className="text-[20px]/[24px] font-[700] poppins">Travel Summary</h1>
+              <h1 className="text-[20px]/[24px] font-[700] poppins">Warehouse Management
+</h1>
               <h1 className="text-[14px]/[24px] font-[600px] mb-10 poppins">
                 <span className="text-[10px]">#{booking?.id}</span> {vehicle?.manufacturer} {vehicle?.model}
               </h1>
